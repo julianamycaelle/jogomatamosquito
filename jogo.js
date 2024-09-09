20dec80e1d966944b9a85f9a07ada0e1842f6b36
julianamycaelle <82950865+julianamycaelle@users.noreply.github.com>
2024-09-09 10:39:41 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas=1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -14,12 +15,21 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
-
 	//remover o mosquito anterior (caso exista)
-	if(document.getElementById('mosquito')) {
-		document.getElementById('mosquito').remove()
+if(document.getElementById('mosquito')) {
+	document.getElementById('mosquito').remove()
+}
+
+//para iniciar a contagem das vidas
+	if(vidas>3){
+		alert('interromper o jogo, game over!')
+	}else{
+		document.getElementById('v'+vidas).src=
+		"imagens/coracao_vazio.png"
+		vidas++
 	}
 
+	
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -36,6 +46,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick=function(){this.remove()
+		vidas--
+	}
 
 	document.body.appendChild(mosquito)
 
@@ -68,4 +81,5 @@ function ladoAleatorio() {
 
 	}
 }
+
 
