446f24400715bcd03c14098a6bb919e63e865a37
julianamycaelle <82950865+julianamycaelle@users.noreply.github.com>
2024-09-11 10:07:05 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -2,6 +2,19 @@ var altura = 0
 var largura = 0
 var vidas=1
 var tempo=15
+var criaMosquitoTempo=1500
+
+var nivel=window.location.search
+nivel=nivel.replace("?,")
+
+if(nivel=='normal'){
+	criaMosquitoTempo=1500
+}else if(nivel =='dificil'){
+	criaMosquitoTempo=1000
+}else if(nivel=='hard'){
+	criaMosquitoTempo=750
+}
+
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
